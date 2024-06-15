8608aceea0b0c2ad6af2f853cdf5d47c573eb6d6
BeLieVer031 <sandyrajak031@gmail.com>
2024-06-15 23:33:22 +0530

    add .env

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+import { config } from "dotenv";
+config();
+
+const Config = process.env;
+export default Config;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
-function display(username: string): void {
-    console.log(username);
-    const p = {
-        age: 19,
-    };
+import app from "./app";
+import Config from "./config";
 
-    console.log(p.age);
-}
+const startServer = () => {
+    try {
+        const PORT = Config.PORT;
+        app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
 
-display("username");
+startServer();
