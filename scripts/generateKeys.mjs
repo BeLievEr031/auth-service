944e454017b73b3448004eaa4e9cc6e8ad2e3633
BeLieVer031 <sandyrajak031@gmail.com>
2024-07-08 18:27:17 +0530

    formate mjs

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,20 +1,18 @@
-import { generateKeyPairSync } from 'node:crypto';
-import fs from 'node:fs';
+import { generateKeyPairSync } from "node:crypto";
+import fs from "node:fs";
 
-const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+const { publicKey, privateKey } = generateKeyPairSync("rsa", {
     modulusLength: 2048,
     publicKeyEncoding: {
-        type: 'spki', // Recommended to use 'spki' for public keys
-        format: 'pem'
+        type: "spki", // Recommended to use 'spki' for public keys
+        format: "pem",
     },
     privateKeyEncoding: {
-        type: 'pkcs8', // Recommended to use 'pkcs8' for private keys
-        format: 'pem',
-    }
+        type: "pkcs8", // Recommended to use 'pkcs8' for private keys
+        format: "pem",
+    },
 });
 
 // Save the keys to files
-fs.writeFileSync('../certs/private_key.pem', privateKey);
-fs.writeFileSync('../certs/public_key.pem', publicKey);
-
-
+fs.writeFileSync("../certs/private_key.pem", privateKey);
+fs.writeFileSync("../certs/public_key.pem", publicKey);
