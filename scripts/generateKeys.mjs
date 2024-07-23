7981f5e5a66dfc4159267d1aaee8a9cf7e55d78a
BeLieVer031 <sandyrajak031@gmail.com>
2024-07-23 21:27:07 +0530

    update code generate pem keys

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,18 +1,17 @@
-import { generateKeyPairSync } from "node:crypto";
-import fs from "node:fs";
+import crypto from "crypto";
+import fs from "fs";
 
-const { publicKey, privateKey } = generateKeyPairSync("rsa", {
+const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
     modulusLength: 2048,
     publicKeyEncoding: {
-        type: "spki", // Recommended to use 'spki' for public keys
+        type: "pkcs1",
         format: "pem",
     },
     privateKeyEncoding: {
-        type: "pkcs8", // Recommended to use 'pkcs8' for private keys
+        type: "pkcs1",
         format: "pem",
     },
 });
 
-// Save the keys to files
-fs.writeFileSync("../certs/private_key.pem", privateKey);
-fs.writeFileSync("../certs/public_key.pem", publicKey);
+fs.writeFileSync("certs/private.pem", privateKey);
+fs.writeFileSync("certs/public.pem", publicKey);
